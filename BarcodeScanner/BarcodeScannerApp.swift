e23b435729f759c2d225a8db0525f22bcd20e16e
Mladen <pasaramladen14@gmail.com>
2025-06-17 12:08:56 +0200

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Mladen Pašara on 17.06.2025..
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
